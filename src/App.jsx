fc73a61005adb7a5bf1033145e43726b78742934
Oluwafemi Ojetokun <ojetokunakinwale@gmail.com>
2023-06-24 19:20:47 +0100

    add: logo for website

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // remember to credit icons8 for the star wars logos
+  // <a target="_blank" href="https://icons8.com/icon/21576/star-wars">Star Wars</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
+      <p className="text-3xl text-gray-700 font-bold mb-5">
+        Welcome!
       </p>
-    </>
+      <p className="text-gray-500 text-lg">
+        React and Tailwind CSS in action
+      </p>
+    </div>
   )
 }
 
